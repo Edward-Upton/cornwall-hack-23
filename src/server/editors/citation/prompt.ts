20c8f8ceee038bdcd8f4aa79518310dafbbef24d
Edward Upton <ed.upton@outlook.com>
2023-10-29 15:20:12 +0000

    Add openai wrapper

--- src/server/editors/citation/prompt.ts
+++ src/server/editors/citation/prompt.ts
@@ -1,28 +1,27 @@
 // This code is for v4 of the openai package: npmjs.com/package/openai
-import OpenAI from "openai";
+import { query } from "~/server/openai-wrapper";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 export const getCitation = async (input: string) => {
-    const response = await openai.chat.completions.create({
-        model: "gpt-4",
-        messages: [
-            {
-                "role": "system",
-                "content": "You will be given a block of text about a subject. You'll also be given some contextual information that is something to do with the text, as well as where that contextual information came from. I'd like you to modify the text to cite the information.\n"
-            },
-            {
-                "role": "user",
-                "content": input
-            }
-        ],
-        temperature: 1,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,    });
+  const response = await query({
+    model: "gpt-4",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You will be given a block of text about a subject. You'll also be given some contextual information that is something to do with the text, as well as where that contextual information came from. I'd like you to modify the text to cite the information.\n",
+      },
+      {
+        role: "user",
+        content: input,
+      },
+    ],
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
 
-    return response;
-}
+  return response;
+};
