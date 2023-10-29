ce549967480c20b2da7e5987faaa7892b7429a6e
Joe <joehewett1@gmail.com>
2023-10-29 11:33:46 +0000

    Add EditEvent const

--- src/lib/editors-types.ts
+++ src/lib/editors-types.ts
@@ -1,4 +1,31 @@
+import { AlignVerticalSpaceAround, List, LucideIcon } from "lucide-react";
 import { z } from "zod";
 
 export const EditorTypes = z.enum(["expansion", "points", "summarise"]);
 export type EditorType = z.infer<typeof EditorTypes>;
+
+export interface EditEvent {
+  input: string;
+  output: string;
+  editType: EditorType;
+};
+
+export const Editors: {
+  display: string;
+  value: EditorType;
+  icon: LucideIcon;
+  color: string;
+}[] = [
+  {
+    display: "Expand",
+    value: "expansion",
+    icon: List,
+    color: "bg-slate-400",
+  },
+  {
+    display: "Summarise",
+    value: "summarise",
+    icon: AlignVerticalSpaceAround,
+    color: "bg-gray-200",
+  },
+];
