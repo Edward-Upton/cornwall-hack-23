b77ec75d9d706f08622fe9bdc6e0945d4a8b69b3
Joe <joehewett1@gmail.com>
2023-10-30 23:14:20 +0000

    Optimise for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <Nav />
-            <div className="flex-1 px-32 py-32">{children}</div>
+            <div className="flex-1 p-8 md:p-32">{children}</div>
           </ThemeProvider>
         </TRPCReactProvider>{" "}
       </body>
